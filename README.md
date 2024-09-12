0f1d80edf3abf0abb19f7bed239c56dee313f1f1
NamanSharmaPersonal <namansharma18899@gmail.com>
2024-09-12 13:45:15 +0530

    Added Go Multithreading spearhead...

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# gopher-bench
+<!-- # gopher-bench -->
+<div align='center'>
+<img src="assets/images/gopher-shoot.webp" width=75%  height=300px>
+<hr>
+
+### **✨ Your Personal Benchmarking Tool✨**
+
+![Code Size](https://img.shields.io/github/languages/code-size/namansharma18899/gopher-bench)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/namansharma18899/gopher-bench/blob/main/LICENSE)
+[![GitHub forks](https://img.shields.io/github/forks/namansharma18899/gopher-bench)](https://github.com/namansharma18899/gopher-bench/network)
+[![GitHub issues](https://img.shields.io/github/issues/namansharma18899/gopher-bench)](https://github.com/namansharma18899/gopher-bench/issues)
+[![Last Commit](https://img.shields.io/github/last-commit/namansharma18899/gopher-bench)](https://github.com/namansharma18899/gopher-bench/commits/main)
+
+*If you like my work a ⭐ would mean the world*
+</div>
+
+---
+
+## 🌎 Overview
+The gopher-bench is a benchmarking tool written in Go
+
+## 🫧 Features
+- **Beginner Friendly**: Easy to use & configure.
+
+- **Go Power**: Developed to leverage Go's multithreading...
+
+## 🤨 Why
+- Inspired by WRK2 : )
+
+## ⚙️ Setup
+```bash
+$ cd gopher-bench/
+$ go get github.com/olekukonko/tablewriter
+$ go run .
+```
+
+## 🤗 Support
+- Make sure to leave a ⭐ if you like this project.
--- app.go
+++ app.go
@@ -6,14 +6,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/olekukonko/tablewriter"
 )
-
-type HTTPBench struct {
-	MethodName  
-}
 
 type Config struct {
 	URL        string `json:"url"`
@@ -35,37 +32,55 @@ func loadConfig() (*Config, error) {
 	return config, nil
 }
 
+func benchApp(config Config, ch chan [2]float64) {
+	start := time.Now()
+	resp, err := http.Get(config.URL)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	defer resp.Body.Close()
+	elapsed := time.Since(start)
+	latency := float64(elapsed.Milliseconds())
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	size := len(body)
+	throughput := float64(size) / latency / 125
+	var arr [2]float64
+	arr[0] = latency
+	arr[1] = throughput
+	ch <- arr
+}
+
 func main() {
 	config, err := loadConfig()
 	if err != nil {
 		panic(err)
 	}
-
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"URL", "Latency", "Throughput(Mbps)"})
 
-	for i := 0; i < config.Iterations; i++ {
-		start := time.Now()
-		resp, err := http.Get(config.URL)
-		if err != nil {
-			fmt.Println("Error:", err)
-			continue
-		}
-		defer resp.Body.Close()
+	threads := 10
+	ch := make(chan [2]float64, threads)
+	var wg sync.WaitGroup
+	for i := 1; i <= threads; i++ {
+		wg.Add(1)
+		go func(config Config, ch chan [2]float64) {
+			benchApp(config, ch)
+			defer wg.Done()
+		}(*config, ch)
+	}
+	go func() {
+		wg.Wait()
+		defer close(ch)
+	}()
 
-		elapsed := time.Since(start)
-		latency := float64(elapsed.Milliseconds())
-
-		body, _ := ioutil.ReadAll(resp.Body)
-		size := len(body)
-		throughput := float64(size) / latency / 125
-
+	for data := range ch {
+		fmt.Printf("Main thread received data: %d\n", data)
 		table.Append([]string{
 			config.URL,
-			fmt.Sprintf("%.2f ms", latency),
-			fmt.Sprintf("%.2f Mbps", throughput),
+			fmt.Sprintf("%.2f ms", data[0]),
+			fmt.Sprintf("%.2f Mbps", data[1]),
 		})
 	}
-
 	table.Render()
 }
